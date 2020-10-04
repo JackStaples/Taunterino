d8fdaf35546988dd17ccfd8526a677170f5334d3
Jack Staples <JackStaplesjcrs@gmail.com>
2020-10-04 12:30:44 -0700

    Add toggle button to options interface. add stubbed functions to options interface okay and cancel buttons

--- main.lua
+++ main.lua
@@ -1,8 +1,25 @@
 
-local frame = CreateFrame("MessageFrame")
-frame.name = "Taunterino"
-InterfaceOptions_AddCategory(frame)
+local interface = CreateFrame("Frame", "TaunterinoConfig", UIParent)
+
+local inCombatButton = CreateFrame("CheckButton", "inCombatButton", interface, "OptionsBaseCheckButtonTemplate")
+inCombatButton:SetPoint("TOPLEFT", 0, 0);
+--inCombatButton_GlobalNameText:SetText("Disable in Combat");
+--getglobal(inCombatButton:GetName() .. 'Text'):SetText("CheckBox Name");
+print(inCombatButton)
+
+interface.name = "Taunterino"
+interface.okay = function()
+    print("Okay")
+end
+interface.cancel = function()
+    print("Canceling")
+end
+InterfaceOptions_AddCategory(interface)
+
+
+
 -- This handles the actual taunting
+local frame = CreateFrame("Frame", "Taunterino")
 frame:RegisterEvent("CHAT_MSG_GUILD")
 local function guildMessageHandler(self, event, message, ...)
     if (string.match(message, '^[0-9]+$'))
